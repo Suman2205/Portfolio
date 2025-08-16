9f438027906e1fbd9b88bd970bc5bf2f76f9b32f
Suman2205 <suman27574@gmail.com>
2025-08-16 20:28:10 +0530

    Initial Commit

--- src/Project.jsx
+++ src/Project.jsx
@@ -2,7 +2,7 @@ import ProjectCard from "./ProjectCard";
 import './Project.css';
 export default function Project(){
     let images=["/ProjectImg4.png","/ProjectImg3.png","/ProjectImg1.jpg","/ProjectImg6.png","/ProjectImg5.png","/ProjectImg7.png"];
-    let links=["https://suman2205.github.io/QuizApplication/","https://suman2205.github.io/PerfectGuessGame/","https://chatapplication-6e1x.onrender.com","https://majorproject-3ysx.onrender.com","https://studyadda.onrender.com/","/project"];
+    let links=["https://suman2205.github.io/QuizApplication/","https://suman2205.github.io/PerfectGuessGame/","https://chatapplication-6e1x.onrender.com","https://majorproject-3ysx.onrender.com","https://studyadda.onrender.com/","https://portfolio-mxzw.onrender.com/"];
     let titles=["Interactive Quiz Application","Perfect Guess Game","ChatNova","WanderLust","StudyAdda","Portfolio"];
     let contents=["A multi-category quiz app with real-time scoring and instant feedback for an engaging learning experience.","A number guessing game that challenges logic and luck within limited attempts, offering fun while boosting problem-solving skills.","A real-time group chat app with unique room IDs, auto-reconnect, message sync, and secure history deletion.","A travel web app to explore, create, and review stays, featuring authentication and smooth image handling.","A student resource hub with notes, past papers, practicals, and books in one organized platform.","A responsive React + Bootstrap portfolio showcasing my projects, skills, and achievements with clean design and easy navigation. Built to provide a quick snapshot of my work and growth as a developer."];
     return(
